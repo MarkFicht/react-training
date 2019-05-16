dc15bfcc137959d400456ec77cd99c9b58ceef6f
Marek <marekficht@gmail.com>
2019-05-16 15:32:09 +0200

    todo-feat: React hooks, refactor NotFound component

--- todo-feat/src/components/NotFound/index.js
+++ todo-feat/src/components/NotFound/index.js
@@ -1,34 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 
-class NotFound extends Component {
-    state = {
-        counter: 3
-    }
+const NotFound = ({ location }) => {
+    const [counter, setCounter] = useState(3)
+    // const [intervalId, setIntervalId] = useState()
+    const countdown = () => setCounter(counter - 1)
 
-    componentDidMount = () => {
+    useEffect(() => {
+        // console.log('cDM or cDU')
 
-        const intervalId = setInterval(this.countdown, 1000)
+        // Bad
+        // const intevral = setInterval(countdown, 1000)
 
-        this.setState({ intervalId })
-    }
+        // good
+        // const intevral = setInterval(countdown, 1000)
+        // setIntervalId(intevral)
 
-    countdown = () => this.setState({ counter: this.state.counter - 1 })
+        // return () => {
+        //     console.log('cWU')
+        //     clearInterval(intevral)
+        // }
 
-    componentWillUnmount = () => clearInterval(this.state.intervalId)
+        const id = setTimeout(countdown, 1000)
 
-    render() {
-        const { location } = this.props
-        const { counter } = this.state
+        return () => clearTimeout(id)
 
-        return (
-            <div>
-                <p>No match for <code>{location.pathname}</code></p>
-                <p>Redirect to home in: {counter}</p>
-                {counter === 0 && <Redirect to='/' />}
-            </div>
-        )
-    }
+    }, [counter]) // condition when re-render
+
+    return (
+        <div>
+            <p>No match for <code>{location.pathname}</code></p>
+            <p>Redirect to home in: {counter}</p>
+            {counter === 0 && <Redirect to='/' />}
+        </div>
+    )
+
 }
+
+// class NotFound extends Component {
+//     state = {
+//         counter: 3
+//     }
+
+//     componentDidMount = () => {
+
+//         const intervalId = setInterval(this.countdown, 1000)
+
+//         this.setState({ intervalId })
+//     }
+
+//     countdown = () => this.setState({ counter: this.state.counter - 1 })
+
+//     componentWillUnmount = () => clearInterval(this.state.intervalId)
+
+//     render() {
+//         const { location } = this.props
+//         const { counter } = this.state
+
+//         return (
+//             <div>
+//                 <p>No match for <code>{location.pathname}</code></p>
+//                 <p>Redirect to home in: {counter}</p>
+//                 {counter === 0 && <Redirect to='/' />}
+//             </div>
+//         )
+//     }
+// }
 
 export default NotFound
